5bba089772efc01dfccd892bb0f86d7dd8e0c5a6
CenTea <34390309+CenTea@users.noreply.github.com>
2020-02-29 18:35:24 -0800

    BST Finding

---
+++ BST.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+
+class BST {
+public:
+  int value;
+  BST *left;
+  BST *right;
+
+  BST(int val);
+  BST &insert(int val);
+};
+
+int findClosestValueInBst(BST *tree, int target) {
+  // Write your code here.
+	int distance = abs(target - tree->value);
+	int closestValue = tree->value;
+	
+	while(true){
+		if(abs(target - tree->value) < distance){
+			distance = abs(target - tree->value);
+			closestValue = tree->value;
+		}
+		if(tree->value == target){
+			cout<<"yay we found it";
+			return tree->value;
+		}
+		else if(tree->value < target){
+			if(tree->right != NULL){
+				tree = tree->right;
+			}
+			else{
+				return closestValue;
+			}
+			
+		}
+		else{
+			if(tree->left != NULL){
+				tree = tree->left;
+			}
+			else{
+				return closestValue;
+			}
+		}
+	}
+return -1;
+}
